e493edde42ea0ea6e7dbf7d6ae14996187be2dd9
dev <git@dev.0x8.in.th>
2025-12-02 13:01:09 +0700

    pixel.php for session maintaner

---
+++ Assets/pixel.php
@@ -0,0 +1,17 @@
+<?php
+// this sends the pixel image but also re-loads the current session
+
+/*
+ * include it like this 
+
+<img src="/pixel.php?ts=<?=time()?>" style="display:none" id="ka">
+<script>
+setInterval(() => {
+    document.getElementById("ka").src = "/keepalive.php?ts=" + Date.now();
+}, 150000); // 2,5 minutes
+</script>
+
+*/
+session_start();
+header("Content-Type: image/gif");
+echo base64_decode("R0lGODlhAQABAPAAAP///wAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==");
