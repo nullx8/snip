6b2516b6aeb23e0cc3e463b6669a076b7766f151
dev <git@dev.0x8.in.th>
2025-12-18 00:41:28 +0700

    debug =0

--- 3rd/fixer.io/fixer.io.inc.php
+++ 3rd/fixer.io/fixer.io.inc.php
@@ -5,7 +5,7 @@
 // and call fxRate($from,$to, $round = null);
 
 
-error_reporting(E_ALL); ini_set('display_errors', '1'); ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL); ini_set('display_errors', '1'); ini_set('display_startup_errors', '1');
 
 // if (!defined('CACHE')) { die("No Direct access"); }
 
--- Math/fxrate.inc.php
+++ Math/fxrate.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-error_reporting(E_ALL); ini_set('display_errors', '1'); ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL); ini_set('display_errors', '1'); ini_set('display_startup_errors', '1');
 
 if (!function_exists('fixerFxRate')) {
 	require_once(__DIR__.'/../3rd/fixer.io/fixer.io.inc.php');
@@ -10,4 +10,5 @@ function fxRate($from, $to, $round = null) {
 	return fixerFxRate($from, $to, $round);
 }
 
-print_r(fxRate('EUR','usD'));
+// print_r(fxRate('EUR','usD'));
+// print_r(fxRate('EUR','THB',2));
