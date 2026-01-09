d8fb37383bac77c1118fe33b2a4043fc2d17f785
nullx8 <mario@hennenberger.de>
2026-01-09 13:07:24 +0700

    ajust paths for ./Test

--- Ext/Test/deepseek-chat.php
+++ Ext/Test/deepseek-chat.php
@@ -3,9 +3,9 @@
 if($_SERVER['REMOTE_ADDR']!= '172.16.67.28') { die("nope"); }
 if(empty($_REQUEST['q'])) { die("question required"); }
 
-$k = trim(file_get_contents(__DIR__.'/../3rd/deepseek/.Token'));
+$k = trim(file_get_contents(__DIR__.'/../../3rd/deepseek/.Token'));
 
-require_once(__DIR__.'/../3rd/deepseek/vendor/autoload.php');
+require_once(__DIR__.'/../../3rd/deepseek/vendor/autoload.php');
 
 use DeepSeek\DeepSeekClient;
 
